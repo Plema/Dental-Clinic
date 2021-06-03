9c275f7ce5787d5cc03f6767887cb710cfd7161c
Anton Milasenko <48668353+Miant1@users.noreply.github.com>
2021-06-03 17:55:28 +0300

    last changes from 3.06

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-$()  
+$(document).ready(function(){
+
+    // Result slider
+    $('.result-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        nextArrow: '<button class="slick-arrow next"><i class="icon-next"></i></button>',
+        prevArrow: '<button class="slick-arrow prev"><i class="icon-prev"></i></button>',
+        dots: true 
+    });
+
+});
